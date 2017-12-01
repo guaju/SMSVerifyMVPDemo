8628f1edde16fe4eee7ccde0544427f996c3afdd
guaju <guaju@126.com>
2017-12-01 09:33:49 +0800

    post

---
+++ app/src/main/java/com/guaju/smsverifymvpdemo/model/engine/LoginEngine.java
@@ -0,0 +1,8 @@
+package com.guaju.smsverifymvpdemo.model.engine;
+
+/**
+ * Created by guaju on 2017/12/1.
+ */
+
+public class LoginEngine {
+}
---
+++ app/src/test/java/com/guaju/smsverifymvpdemo/TestMethod.java
@@ -0,0 +1,8 @@
+package com.guaju.smsverifymvpdemo;
+
+/**
+ * Created by guaju on 2017/11/30.
+ */
+
+public class TestMethod {
+}
